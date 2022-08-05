ecc708ed1bda13195224b9ef6b9226f0a81ca5a4
shubham <shubham.kumar@geeky.dev>
2022-08-05 13:39:40 +0530

    policy create added on qr code

--- src/controller/auth/apiAuth.js
+++ src/controller/auth/apiAuth.js
@@ -9,14 +9,34 @@ const googleCloudApi = {
     async genrateUniqueQR(req, res, next) {
         const authClient = await Auth.getClient();
         google.options({ auth: authClient });
+        const policyName = `enterprises/LC03o7l0pz/policies/policyId_${new Date().getTime().toString()}`
+        //                  Create Policies                 //
+        const policy_json = {
+            "applications": [
+                {
+                    "packageName": "com.google.samples.apps.iosched",
+                    "installType": "FORCE_INSTALLED"
+                }
+            ],
+            "debuggingFeaturesAllowed": true
+        }
+
+        const createPolicies = await androidManagement.enterprises.policies.patch({
+            name: `${policyName}`,
+            updateMask: 'applications',
+            requestBody: policy_json
+        })
+        console.log("policyName",createPolicies.data.name)
+        //                  Create Policies End                //
+
         const enrollmentToken = await androidManagement.enterprises.enrollmentTokens.create({
             parent: "enterprises/LC03o7l0pz",
             requestBody: {
-                policyName: "enterprises/LC03o7l0pz/policies/daishygoyal"
+                policyName: `${createPolicies.data.name}`
             }
         })
-        const genratedList = await QRCodeUrl(enrollmentToken)
-       
+       const genratedList = await QRCodeUrl(enrollmentToken)
+
         res.json({ message: 'success', data: genratedList })
     },
 
@@ -24,7 +44,7 @@ const googleCloudApi = {
         const authClient = await Auth.getClient();
         google.options({ auth: authClient });
         const getDevices = await androidManagement.enterprises.devices.list({
-            parent: "enterprises/LC03o7l0pz"
+            parent: "enterprises/LC03o7l0pz"  // Enterprise Name
         })
         res.json({ message: 'success', data: getDevices.data })
     },
@@ -34,7 +54,7 @@ const googleCloudApi = {
         google.options({ auth: authClient });
 
         const reboot = await androidManagement.enterprises.devices.issueCommand({
-            name: "enterprises/LC03o7l0pz/devices/31dc21440073fbc1",
+            name: "enterprises/LC03o7l0pz/devices/31dc21440073fbc1", // new Device Realme 7A plociyId => 369e6387affd0d7a
             requestBody: {
                 type: "REBOOT"
             }
@@ -46,7 +66,7 @@ const googleCloudApi = {
         const authClient = await Auth.getClient();
         google.options({ auth: authClient });
 
-        const lock = await androidManagement.enterprises.policies.patch({
+        const lockApp = await androidManagement.enterprises.policies.patch({
             name: "enterprises/LC03o7l0pz/policies/daishygoyal",
             updateMask: "applications",
             requestBody: {
@@ -58,10 +78,11 @@ const googleCloudApi = {
                 ]
             }
         })
-        res.json({ message: 'App Locked Successfull', data: lock })
+        res.json({ message: 'App Locked Successfull', data: lockApp })
     }
 }
 export default googleCloudApi
+
 
 
 
